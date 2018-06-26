b99685b446c5dd51765c77ef7217d3d0883e75b7
Winhyoni <Winhyoni@DESKTOP-ATV4CU2>
2018-06-26 20:42:59 +0900

    test4

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -66,4 +66,9 @@ public class UserController {
 		ModelAndView mav = new ModelAndView();
 		return mav;
 	}
+	@RequestMapping("user/selectJoin")
+	public ModelAndView test4() {
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
 }
